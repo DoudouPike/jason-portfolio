24c7635b0ba603034fec7fdfab885986091fc8d6
Kévin <doudoupike@hotmail.fr>
2018-11-13 12:36:15 +0100

    Update adminpanel

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,13 +5,15 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
+// any CSS you require will output into a single css file (admin.css in this case)
 require('bootstrap/dist/css/bootstrap.min.css');
 require('@fortawesome/fontawesome-free/css/regular.min.css');
 require('@fortawesome/fontawesome-free/css/solid.min.css');
 require('@fortawesome/fontawesome-free/css/fontawesome.min.css');
-require('../scss/app.scss');
-require('../css/app.css');
+require('../css/dark-admin1-4-4/font.css');
+require('../css/dark-admin1-4-4/style.default.css');
+require('../css/dark-admin1-4-4/custom.css');
+require('../css/admin.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
