8f45a91385aa8f63fa651f17ea3ec6c73a9e0a0d
Unknown <doudoupike@hotmail.fr>
2018-11-13 17:31:53 +0100

    Update + need fix for bootstrap dropdown

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,16 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you require will output into a single css file (admin.css in this case)
-require('bootstrap/dist/css/bootstrap.min.css');
-require('@fortawesome/fontawesome-free/css/regular.min.css');
-require('@fortawesome/fontawesome-free/css/solid.min.css');
-require('@fortawesome/fontawesome-free/css/fontawesome.min.css');
-require('../css/dark-admin1-4-4/font.css');
+require('../img/flags/16/FR.png');
+require('../img/flags/16/GB.png');
 require('../css/dark-admin1-4-4/style.default.css');
 require('../css/dark-admin1-4-4/custom.css');
 require('../css/admin.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
-// any CSS you require will output into a single css file (app.css in this case)
-require('bootstrap/dist/css/bootstrap.min.css');
-require('@fortawesome/fontawesome-free/css/regular.min.css');
-require('@fortawesome/fontawesome-free/css/solid.min.css');
-require('@fortawesome/fontawesome-free/css/fontawesome.min.css');
+const $ = require('jquery');
+require('bootstrap');
+require('popper.js');
 require('../scss/app.scss');
+// any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// var $ = require('jquery');
