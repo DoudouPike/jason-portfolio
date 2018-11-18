652877a4f4833b6c2a86718ee5313d0e23ba3a49
Unknown <doudoupike@hotmail.fr>
2018-11-18 02:09:48 +0100

    Fix some webpack issue

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,11 +2,12 @@
  * Welcome to your app's main JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * (and its CSS file) in your base layout (layout.html.twig).
  */
 // any CSS you require will output into a single css file (admin.css in this case)
 require('../img/flags/16/FR.png');
 require('../img/flags/16/GB.png');
-require('../css/dark-admin1-4-4/style.default.css');
-require('../css/dark-admin1-4-4/custom.css');
+require('../js/front');
+require('../css/style.default.css');
+require('../css/custom.css');
 require('../css/admin.css');
